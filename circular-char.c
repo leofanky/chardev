cd1804bd540f7901926098c46575bb80e0457721
leofanky <fillow99@gmail.com>
2018-12-17 19:29:34 +0100

    ssss

--- circular-char.c
+++ circular-char.c
@@ -1,121 +1,140 @@
-/*
- * circular-char.c
- *
- * Copyright (C) 2012 Opersys inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License, not any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
+#include <linux/version.h>
+#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/miscdevice.h>
 #include <linux/fs.h>
-#include <asm/uaccess.h>
-
-#define BUF_SIZE 200
-
-static char buf[BUF_SIZE];
-static char *read_ptr;
-static char *write_ptr;
-
-static int device_open(struct inode *inode, struct file *file)
-{
-  printk("device_open called \n");
-
-  return 0;
-}
-
-static int device_release(struct inode *inode, struct file *file)
-{
-  printk("device_release called \n");
-
-  return 0;
-}
-
-static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
-			   char *buffer,	/* buffer to fill with data */
-			   size_t length,	/* length of the buffer     */
-			   loff_t * offset)
-{
-  int chars_read = 0;
-
-  printk("device_read called \n");
-
-  while(length && *read_ptr && (read_ptr != write_ptr)) {
-    put_user(*(read_ptr++), buffer++);
-
-    printk("Reading %c \n", *read_ptr);
-
-    if(read_ptr >= buf + BUF_SIZE)
-      read_ptr = buf;
-
-    chars_read++;
-    length--;
-  }
-
-  return chars_read;
-}
-
-static ssize_t
-device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
-{
-  int i;
-
-  printk("device_write called \n");
-
-  for(i = 0; i < len; i++) {
-    get_user(*write_ptr, buff++);
-    printk("Writing %c \n", *write_ptr);
-    write_ptr++;
-    if (write_ptr >= buf + BUF_SIZE)
-      write_ptr = buf;
-  }
-
-  return len;
-}
-
-static struct file_operations fops = {
-  .open = device_open,
-  .release = device_release,
-  .read = device_read,
-  .write = device_write,
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/mm.h>
+#include <linux/dma-mapping.h>
+#ifdef MODVERSIONS
+#include <linux/modversions.h>
+#endif
+#include <asm/io.h>
+static dev_t mmap_dev;
+static struct cdev mmap_cdev;
+// methods of the character device
+static int mmap_open(struct inode *inode, struct file *filp);
+static int mmap_release(struct inode *inode, struct file *filp);
+static int mmap_mmap(struct file *filp, struct vm_area_struct *vma);
+// file operations
+static struct file_operations mmap_fops = {
+        .open = mmap_open,
+        .release = mmap_release,
+        .mmap = mmap_mmap,
+        .owner = THIS_MODULE,
 };
-
-static struct miscdevice circ_char_misc = {
-  .minor = MISC_DYNAMIC_MINOR,
-  .name = "circchar",
-  .fops = &fops,
-};
-
-int circ_char_enter(void)
+// length of the two memory areas
+#define NPAGES 16
+// pointer to the allocated area, rounded up to a page boundary
+static int *alloc_area;
+// original pointer for allocated area
+static void *alloc_ptr;
+dma_addr_t dma_handle;
+// character device open method
+static int mmap_open(struct inode *inode, struct file *filp)
 {
-  int retval;
-
-  retval = misc_register(&circ_char_misc);
-  printk("CIRC Driver got retval %d\n", retval);
-  printk("mmap is %08X\n", (int) fops.mmap);
-
-  read_ptr = buf;
-  write_ptr = buf;
-
-  return 0;
+	printk(KERN_INFO "mmap_alloc: device open\n");
+        return 0;
 }
-
-void circ_char_exit(void)
+// character device last close method
+static int mmap_release(struct inode *inode, struct file *filp)
 {
-  misc_deregister(&circ_char_misc);
+	printk(KERN_INFO "mmap_alloc: device is being released\n");
+        return 0;
 }
-
-module_init(circ_char_enter);
-module_exit(circ_char_exit);
+int mmap_kmem(struct file *filp, struct vm_area_struct *vma)
+{
+        int ret;
+        long length = vma->vm_end - vma->vm_start;
+        // check length
+        if (length > NPAGES * PAGE_SIZE)
+                return -EIO;
+	if (vma->vm_pgoff == 0) {
+		printk(KERN_INFO "Using dma_mmap_coherent\n");
+		ret = dma_mmap_coherent(NULL, vma, alloc_ptr,
+					dma_handle, length);
+	} else
+	{
+		printk(KERN_INFO "Using remap_pfn_range\n");
+		vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+		vma->vm_flags |= VM_IO;
+		printk(KERN_INFO "off=%d\n", vma->vm_pgoff);
+	        ret = remap_pfn_range(vma, vma->vm_start,
+			      PFN_DOWN(virt_to_phys(bus_to_virt(dma_handle))) +
+			      vma->vm_pgoff, length, vma->vm_page_prot);
+	}
+        // map the whole physically contiguous area in one piece
+        if (ret < 0) {
+		printk(KERN_ERR "mmap_alloc: remap failed (%d)\n", ret);
+		return ret;
+        }
+        return 0;
+}
+// character device mmap method
+static int mmap_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	printk(KERN_INFO "mmap_alloc: device is being mapped\n");
+        return mmap_kmem(filp, vma);
+}
+// module initialization - called at module load time
+static int __init mmap_alloc_init(void)
+{
+        int ret = 0;
+        int i;
+	// Allocate not-cached memory area with dma_map_coherent.
+	printk(KERN_INFO "Use dma_alloc_coherent\n");
+	alloc_ptr = dma_alloc_coherent (NULL, (NPAGES + 2) * PAGE_SIZE,
+					&dma_handle, GFP_KERNEL);
+	if (!alloc_ptr) {
+                printk(KERN_ERR
+		    "mmap_alloc: dma_alloc_coherent error\n");
+                ret = -ENOMEM;
+                goto out;
+        }
+	printk(KERN_INFO "mmap_alloc: physical address is %lu\n",
+	    dma_handle);
+	printk(KERN_INFO "mmap_alloc: bus_to_virt %lu\n",
+	    virt_to_phys(bus_to_virt(dma_handle)));
+	alloc_area = alloc_ptr;
+        // get the major number of the character device
+        if ((ret = alloc_chrdev_region(&mmap_dev, 0, 1, "mmap_alloc")) < 0) {
+                printk(KERN_ERR
+		    "mmap_alloc: could not allocate major number for mmap\n");
+                goto out_vfree;
+        }
+// initialize the device structure and register the device with the kernel
+        cdev_init(&mmap_cdev, &mmap_fops);
+        if ((ret = cdev_add(&mmap_cdev, mmap_dev, 1)) < 0) {
+                printk(KERN_ERR
+		    "mmap_alloc: could not allocate chrdev for mmap\n");
+                goto out_unalloc_region;
+        }
+// store a pattern in the memory.
+// the test application will check for it
+        for (i = 0; i < (NPAGES * PAGE_SIZE / sizeof(int)); i += 2) {
+                alloc_area[i] = (0xdead << 16) + i;
+                alloc_area[i + 1] = (0xbeef << 16) + i;
+        }
+        return ret;
+  out_unalloc_region:
+        unregister_chrdev_region(mmap_dev, 1);
+  out_vfree:
+	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
+	    dma_handle);
+  out:
+        return ret;
+}
+// module unload
+static void __exit mmap_alloc_exit(void)
+{
+        int i;
+        // remove the character deivce
+        cdev_del(&mmap_cdev);
+        unregister_chrdev_region(mmap_dev, 1);
+	// free the memory areas
+	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
+	    dma_handle);
+}
+module_init(mmap_alloc_init);
+module_exit(mmap_alloc_exit);
